a865f18a01ab18872688eb88131cd09bb7a416dd
SharonMcHugh <sharonmmchugh@gmail.com>
2014-12-02 14:01:03 +0000

    Update testResource.java
    Commented code

--- src/java/resource/testResource.java
+++ src/java/resource/testResource.java
@@ -2,6 +2,7 @@ package resource;
 
 import org.codehaus.groovy.grails.web.json.JSONException;
 
+//class to test resource package
 public class testResource {
 	public static void main (String [ ] args){
 		
